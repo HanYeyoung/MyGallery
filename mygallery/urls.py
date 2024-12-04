d93180345419cee1ebc38af78ce3ae77ac367b55
HanYeyoung <hany2813@naver.com>
2024-12-04 13:52:11 -0600

    add profileapp

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('account/', include('accountapp.urls'))
 ]
